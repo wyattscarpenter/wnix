f34a8fdfa842c76dfa5edf173565dbbc5a9ea45f
wyattscarpenter <wyattscarpenter@gmail.com>
2024-11-15 21:40:23 -0800

    rm #.bat as bad
    ok, so, it turns out, if you naively call a batch file from another batch file, like a # line would do, you can never return to the original file, like we want to at the end of the # 'command'. Weird! As my father says, Windows was made by monkeys

--- #.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-REM This command is a pseudo-comment. It does nothing, so it is like a comment, and will allow you to run (viz: not run) unix⹀shell-commented lines. However, it does not syntactically operate like a comment, so other side⹀effect-producing operations that occur in the line will still occur 😬
